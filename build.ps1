ae6e2a067ad06937b4fd8f493677c69e812e055a
MIDDLEEAST\ityer <itye.richter@microsoft.com>
2018-07-09 11:52:06 +0300

    minor changes to dockerfile

--- build.ps1
+++ build.ps1
@@ -3,12 +3,12 @@ docker tag dotnet-node-alpine ityer/dotnet-node-alpine:net2.1-node9.11-alpine3.7
 docker push ityer/dotnet-node-alpine:net2.1-node9.11-alpine3.7
 
 docker build -t k8s-port-service .\k8s-port-service
-docker tag k8s-port-service ityer/k8s-port-service:v13
-docker push ityer/k8s-port-service:v13
+docker tag k8s-port-service ityer/k8s-port-service:v14
+docker push ityer/k8s-port-service:v14
 
 docker build -t k8s-helm-http-wrapper .\k8s-helm-http-wrapper
-docker tag k8s-helm-http-wrapper ityer/k8s-helm-http-wrapper:v1
-docker push ityer/k8s-helm-http-wrapper:v1
+docker tag k8s-helm-http-wrapper ityer/k8s-helm-http-wrapper:v3
+docker push ityer/k8s-helm-http-wrapper:v3
 
 docker build -t k8s-ingress-manager .\k8s-ingress-manager
 docker tag k8s-ingress-manager ityer/k8s-ingress-manager:v2
