946c238fcf79506a1a0ea51caadac18f5498612f
MIDDLEEAST\ityer <itye.richter@microsoft.com>
2018-07-11 17:21:08 +0300

    build

--- build.ps1
+++ build.ps1
@@ -7,13 +7,9 @@ docker tag k8s-port-service ityer/k8s-port-service:v14
 docker push ityer/k8s-port-service:v14
 
 docker build -t k8s-helm-http-wrapper .\k8s-helm-http-wrapper
-<<<<<<< HEAD
-docker tag k8s-helm-http-wrapper ityer/k8s-helm-http-wrapper:v3
-docker push ityer/k8s-helm-http-wrapper:v3
-=======
 docker tag k8s-helm-http-wrapper ityer/k8s-helm-http-wrapper:v4
 docker push ityer/k8s-helm-http-wrapper:v4
->>>>>>> mergeAll
+
 
 docker build -t k8s-ingress-manager .\k8s-ingress-manager
 docker tag k8s-ingress-manager ityer/k8s-ingress-manager:v2
